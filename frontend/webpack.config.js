5a04728b05624bea923016ac925e8150f4447a40
lucky2bomb <lucky2bomb@gmail.com>
2023-02-22 18:00:33 +0600

    add lint

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,54 +1,58 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 const path = require("path");
+const DotenvWebpack = require('dotenv-webpack');
 
-let mode = "development";
+let mode = "development"
 if (process.env.NODE_ENV === "production") {
-  mode = "production";
+    mode = "production"
 }
 
 module.exports = {
-  mode,
-  entry: "./src/index.tsx",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-  output: {
-    path: path.resolve(__dirname, "./dist"),
-    assetModuleFilename: "assets/[hash][ext][query]",
-    filename: "bundle.js",
-    clean: true,
-    publicPath: "/"
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
-  devtool: "source-map",
-  devServer: {
-    hot: true,
-    static: {
-      directory: path.resolve(__dirname, "./dist"),
-    },
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|ts)x?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            cacheDirectory: true,
-          },
+    mode,
+    entry: "./src/index.tsx",
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
         },
-      },
+    },
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        assetModuleFilename: "assets/[hash][ext][query]",
+        filename: "bundle.js",
+        clean: true,
+        publicPath: "/",
+    },
+    plugins: [
+        new DotenvWebpack({
+            path: ".env",
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
     ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js"],
-  },
-};
+    devtool: "source-map",
+    devServer: {
+        hot: true,
+        static: {
+            directory: path.resolve(__dirname, "./dist"),
+        },
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|ts)x?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        cacheDirectory: true,
+                    },
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".jsx", ".js"],
+    },
+}
