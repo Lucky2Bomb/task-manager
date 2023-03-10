537098e7e72240ef4e5ad5d67087594bf520b354
lucky2bomb <lucky2bomb@gmail.com>
2023-03-10 17:53:45 +0600

    remove "/"

--- backend/main/account/urls.py
+++ backend/main/account/urls.py
@@ -4,9 +4,9 @@ from knox import views as knox_views
 from .views import LoginView, RegisterView, UserView
 
 urlpatterns = [
-     path(r'login/', LoginView.as_view(), name='login'),
-     path(r'register/', RegisterView.as_view(), name='register'),
-     path(r'user/', UserView.as_view(), name="user"),
-     path(r'logout/', knox_views.LogoutView.as_view(), name='logout'),
-     path(r'logout-all/', knox_views.LogoutAllView.as_view(), name='logoutall'),
+     path(r'login', LoginView.as_view(), name='login'),
+     path(r'register', RegisterView.as_view(), name='register'),
+     path(r'user', UserView.as_view(), name="user"),
+     path(r'logout', knox_views.LogoutView.as_view(), name='logout'),
+     path(r'logout-all', knox_views.LogoutAllView.as_view(), name='logoutall'),
 ]
